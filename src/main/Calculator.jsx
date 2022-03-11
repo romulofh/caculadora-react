85e7a657204fe838e51177f8334448a97a5ab2b8
romulofh <romulofurtadoo@gmail.com>
2022-03-11 05:46:34 -0300

    Visu da Calculadora

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,55 @@
+import React, {Component} from "react"
+import './Calculator.css'
+import Button from "../components/Button"
+import Display from "../components/Display"
+
+export default class Calculator extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.clearMemory = this.clearMemory.bind(this)
+    this.setOperation = this.setOperation.bind(this)
+    this.addDigit = this.addDigit.bind(this)
+  }
+
+  clearMemory() {
+    console.log('Limpando!')
+  }
+
+  setOperation(operation) {
+    console.log(operation)
+  }
+
+  addDigit(n) {
+    console.log(n)
+  }
+
+  render() {
+    //As funções tem o mesmo nome para facilitar, mas não são as mesmas ;D
+    const addDigit = n => this.addDigit(n)
+    const setOperation = op => this.setOperation(op)
+    return (
+      <div className="calculator">
+        <Display value={100} />
+        <Button label="AC" click={this.clearMemory} triple />
+        <Button label="/" operation click={this.setOperation} />
+        <Button label="7" click={this.addDigit} />
+        <Button label="8" click={this.addDigit} />
+        <Button label="9" click={this.addDigit} />
+        <Button label="*" operation click={this.setOperation} />
+        <Button label="4" click={this.addDigit} />
+        <Button label="5" click={this.addDigit} />
+        <Button label="6" click={this.addDigit} />
+        <Button label="-" operation click={this.setOperation} />
+        <Button label="1" click={this.addDigit} />
+        <Button label="2" click={this.addDigit} />
+        <Button label="3" click={this.addDigit} />
+        <Button label="+" operation click={this.setOperation} />
+        <Button label="0" click={this.addDigit} double />
+        <Button label="." click={this.addDigit} />
+        <Button label="=" operation click={this.setOperation} />
+      </div>
+    )
+  }
+}
